c6168c5066f4105c7b0e93bc82e63b43a7a02f35
Rafael Santos <prafaelmsantos@gmail.com>
2023-03-30 00:24:48 +0100

    30/03/2023

---
+++ aspnet-mvc-crud-example/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using aspnet_mvc_crud_example.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace aspnet_mvc_crud_example.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View("UserForm");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
