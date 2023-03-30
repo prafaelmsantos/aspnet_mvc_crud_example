d2a3e52a5ee0b96085d71aee7a7ebf28b72665e8
Rafael Santos <prafaelmsantos@gmail.com>
2023-03-30 23:02:43 +0100

    30/03/2023

--- aspnet-mvc-crud-example/Controllers/HomeController.cs
+++ aspnet-mvc-crud-example/Controllers/HomeController.cs
@@ -1,26 +1,56 @@
 ﻿using aspnet_mvc_crud_example.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using System.Diagnostics;
+using System.Linq;
 
 namespace aspnet_mvc_crud_example.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult Register()
+
+        [HttpGet]
+        public IActionResult Register(int? id)
         {
-            return View("UserForm");
+            if (id.HasValue && UserModel.UsersList.Any(u => u.Id == id))
+            {
+                var user = UserModel.UsersList.Single(u => u.Id == id);
+                return View(user);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(UserModel user)
+        {
+            UserModel.Save(user);
+            return RedirectToAction("Users");
+        }
+
+        public IActionResult Users()
+        {
+            return View(UserModel.UsersList);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id.HasValue && UserModel.UsersList.Any(u => u.Id == id))
+            {
+                var user= UserModel.UsersList.Single(u => u.Id == id);
+                return View(user);
+            }
+            return RedirectToAction("Users");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(UserModel user)
+        {
+            UserModel.Delete(user.Id);
+            return RedirectToAction("Users");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ aspnet-mvc-crud-example/Models/UserModel.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace aspnet_mvc_crud_example.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+
+
+        private static List<UserModel> usersList = new List<UserModel>();
+        public static IQueryable<UserModel> UsersList
+        {
+            get
+            {
+                return usersList.AsQueryable();
+            }
+        }
+
+
+        static UserModel()
+        {
+            usersList.Add(
+                new UserModel { Id = 1, Name = "Pedro", Email = "pedro@email.com" });
+            usersList.Add(
+                new UserModel { Id = 2, Name = "Rafael", Email = "rafael@email.com" });
+            usersList.Add(
+                new UserModel { Id = 3, Name = "João", Email = "joao@email.com" });
+            usersList.Add(
+                new UserModel { Id = 4, Name = "Maria", Email = "maria@email.com" });
+                
+        }
+
+        public static void Save(UserModel user)
+        {
+            var userExist = usersList.Find(u => u.Id == user.Id);
+            if (userExist != null)
+            {
+                userExist.Name = user.Name;
+                userExist.Email = user.Email;
+            }
+            else
+            {
+
+                int biggestId = UsersList.Any() ? UsersList.Max(u => u.Id) : 0;
+                user.Id = biggestId + 1;
+                usersList.Add(user);
+            }
+        }
+
+        public static void Delete(int userId)
+        {
+            var userExist = usersList.Find(u => u.Id == userId);
+            if (userExist != null)
+            {
+                usersList.Remove(userExist);
+            }
+        }
+    }
+}
+
